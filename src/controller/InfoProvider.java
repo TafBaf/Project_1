93da5f5c225cc41cd079f3c8690c5dca2d365fdb
Taf <Taf@Taf-PC.lan>
2017-03-30 10:06:55 +0300

    postgres

--- src/controller/InfoProvider.java
+++ src/controller/InfoProvider.java
@@ -1,11 +1,12 @@
 package controller;
 
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService(targetNamespace = "http://controller/", portName = "InfoProviderPort", serviceName = "InfoProviderService")
 public class InfoProvider {
 
-	
+	@WebMethod
 	public String GetInfo(String id) {
 		return "Returned ID: " + id;
 	}
